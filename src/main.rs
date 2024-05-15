2fa6d32112cdf7ce06b6ac67e7494a279d556ff4
ezTxmMC <eztxmmc@hotmail.com>
2024-05-15 21:40:58 +0200

    added setup start only if needed

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,23 @@ fn main() -> Result<()> {
     let main_terminal = CloudTerminal::new("main");
     let setup_terminal = CloudTerminal::new("setup");
 
-    main_terminal.writeline("Welcome to the main terminal.");
-    main_terminal.writeline("Type 'setup' to switch to the setup terminal, 'clear' to clear the screen, or 'exit' to quit.");
-
     let mut current_terminal = main_terminal.get_current_terminal();
+
+    let need_setup = true;
+
+    if need_setup {
+        current_terminal = setup_terminal.get_current_terminal();
+        setup_terminal.writeline("Welcome to the setup terminal.");
+    }
 
     loop {
         let input = current_terminal.readline();
         let current_terminal_name = current_terminal.name.as_str();
 
+        
+
         match current_terminal_name {
             "main" => {
-                if input == "setup" {
-                    current_terminal.change_to_another_terminal(&setup_terminal);
-                    current_terminal = setup_terminal.get_current_terminal();
-                    setup_terminal.writeline("Welcome to the setup terminal.");
-                    setup_terminal.writeline("Type 'main' to switch back to the main terminal, 'clear' to clear the screen, or 'exit' to quit.");
-                    continue;
-                }
                 if input == "clear" {
                     current_terminal.clear()?;
                     continue;
